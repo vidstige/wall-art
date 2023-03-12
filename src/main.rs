cd05706f48444f67bcb09bf1d1698f91cf7faecf
Samuel Carlsson <samuel.carlsson@gmail.com>
2023-03-12 10:54:15 +0100

    lattice

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,41 @@
 use egui;
-use egui::{Frame, Pos2, pos2, vec2, Rect, Stroke, Color32};
+use egui::{Frame, Pos2, Rect, Stroke, Color32};
+use egui::{pos2};
 use egui::epaint;
 use egui::emath;
 
 struct LatticeStudio {
+    points: Vec<Pos2>,
+    edges: Vec<(usize, usize)>,
 }
 
-impl Default for LatticeStudio {
-    fn default() -> Self {
+fn idx(x: usize, y: usize, w: usize) -> usize{
+    y * w + x
+}
+
+impl LatticeStudio {
+    fn new() -> Self {
         Self {
+            points: Vec::new(),
+            edges: Vec::new(),
+        }
+    }
+
+    fn euclidian(&mut self) {
+        // add points
+        let n = 10;
+        for x in 0..n {
+            for y in 0..n {
+                self.points.push(pos2(x as f32 / n as f32, y as f32 / n as f32));
+            }
+        }
+        // add vertices
+        for x in 0..n-1 {
+            for y in 0..n-1 {
+                self.edges.push((idx(x, y, n), idx(x + 1, y, n)));
+                self.edges.push((idx(x, y, n), idx(x, y + 1, n)));
+                self.edges.push((idx(x + 1, y, n), idx(x, y + 1, n)));
+            }
         }
     }
 }
@@ -25,43 +52,31 @@ impl eframe::App for LatticeStudio {
                 ui.ctx().request_repaint();
                 let time = ui.input(|i| i.time);
     
-                let desired_size = ui.available_width() * vec2(1.0, 0.35);
+                let desired_size = ui.available_size();
                 let (_id, rect) = ui.allocate_space(desired_size);
     
                 let to_screen =
-                    emath::RectTransform::from_to(Rect::from_x_y_ranges(0.0..=1.0, -1.0..=1.0), rect);
+                    emath::RectTransform::from_to(Rect::from_x_y_ranges(0.0..=1.0, 0.0..=1.0), rect);
     
+                let thickness = 2.0;
                 let mut shapes = vec![];
-    
-                for &mode in &[2, 3, 5] {
-                    let mode = mode as f64;
-                    let n = 120;
-                    let speed = 1.5;
-    
-                    let points: Vec<Pos2> = (0..=n)
-                        .map(|i| {
-                            let t = i as f64 / (n as f64);
-                            let amp = (time * speed * mode).sin() / mode;
-                            let y = amp * (t * std::f64::consts::TAU / 2.0 * mode).sin();
-                            to_screen * pos2(t as f32, y as f32)
-                        })
-                        .collect();
-    
-                    let thickness = 10.0 / mode as f32;
-                    shapes.push(epaint::Shape::line(points, Stroke::new(thickness, color)));
+                for (i0, i1) in self.edges.iter() {
+                    let points = [to_screen * self.points[*i0], to_screen * self.points[*i1]];
+                    shapes.push(epaint::Shape::line(points.to_vec(), Stroke::new(thickness, color)));
                 }
-    
                 ui.painter().extend(shapes);
             });
         });
     }
 }
 
+fn create_app() -> LatticeStudio {
+    let mut app = LatticeStudio::new();
+    app.euclidian();
+    return app;
+}
 
 fn main() -> Result<(), eframe::Error> {
-    // Log to stdout (if you run with `RUST_LOG=debug`).
-    //tracing_subscriber::fmt::init();
-
     let options = eframe::NativeOptions {
         initial_window_size: Some(egui::vec2(320.0, 240.0)),
         ..Default::default()
@@ -69,6 +84,6 @@ fn main() -> Result<(), eframe::Error> {
     eframe::run_native(
         "Lattice Studio",
         options,
-        Box::new(|_cc| Box::new(LatticeStudio::default())),
+        Box::new(|_cc| Box::new(create_app())),
     )
 }
