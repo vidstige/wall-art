4422af5c599c8957269ef96c6849115e11e7e532
Samuel Carlsson <samuel.carlsson@gmail.com>
2023-04-04 17:24:02 +0200

    delaunay triangulation of random points

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
 import cairo
 import numpy as np
+from scipy.spatial import Delaunay
 
 from mesh_art.gradient import parse_color, sample
 from mesh_art.mesh2d import create_mesh
 
 def main():
-    surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, 200, 200)
+    width, height = 200, 200
+    surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, width, height)
     ctx = cairo.Context(surface)
-    (x, y), triangle_indices = create_mesh((5, 5), (200, 200))
-    
-    x = x + np.random.normal(size=x.shape) * 10
-    y = y + np.random.normal(size=y.shape) * 10
-    gradient = cairo.LinearGradient(0, 0, 200, 200)
+
+    #(x, y), triangle_indices = create_mesh((5, 5), (width, height))
+    #x = x + np.random.normal(size=x.shape) * 10
+    #y = y + np.random.normal(size=y.shape) * 10
+
+    rho = 0.001
+    n = int(rho * width * height)
+    points = np.random.random((n, 2)) * np.array([200, 200])
+    triangle_indices = Delaunay(points).simplices
+    x, y = np.hsplit(points, 2)
 
     # https://uigradients.com/#Neuromancer
+    gradient = cairo.LinearGradient(0, 0, 200, 200)
     gradient.add_color_stop_rgb(0, *parse_color('#f953c6'))
     gradient.add_color_stop_rgb(1, *parse_color('#b91d73'))
     #ctx.set_source(gradient)
